8a91635a3c9952f809e3831a912fc373458b23fc
NhungTranPY <nhungtranpy91@gmail.com>
2023-02-14 16:31:54 +0700

    361. Promise.all(), Promise.race() etc.

--- app.js
+++ app.js
@@ -57,6 +57,20 @@ async function trackUserHandler() {
 
 button.addEventListener('click', trackUserHandler);
 
+Promise.race([getPosition(), setTimer(1000)]).then(data => {
+  console.log(data);
+})
+
+Promise.all([getPosition(), setTimer(1000)]).then(promiseData => {
+  console.log(promiseData);
+})
+
+Promise.allSettled([getPosition(), setTimer(1000)]).then(promisData => {
+  console.log(promisData);
+})
+
+
+
 // let result = 0
 
 // for (let i = 0; i < 100000000; i++) {
