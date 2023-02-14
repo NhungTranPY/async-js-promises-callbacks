e9eec9fd8b44cd7b6eb92d752dd67db7c2bcfb86
NhungTranPY <nhungtranpy91@gmail.com>
2023-02-14 15:32:55 +0700

    358. Async/ await

--- app.js
+++ app.js
@@ -21,25 +21,28 @@ const setTimer = (duration) => {
   return promise
 }
 
-function trackUserHandler() {
+async function trackUserHandler() {
   // console.log('Clicked!');
-  let positionData
-  getPosition()
-    .then(posData => {
-      positionData = posData
-      return setTimer(2000)
-    })
-    .then(data => {
-      console.log(data, positionData);
-    })
-    .catch(err => {
-      console.log(err);
-      return 'on we go...'
-    })
-  setTimer(1000).then(() => {
-    console.log('Timer done!');
-  })
-  console.log('Getting position...');
+  // let positionData
+  const posdata = await getPosition()
+  const timerData = await setTimer(2000)
+  console.log(timerData, posdata);
+    // .then(posData => {
+    //   positionData = posData
+    //   return setTimer(2000)
+    // })
+    // .catch(err => {
+    //   console.log(err);
+    //   return 'on we go...'
+    // })
+    // .then(data => {
+    //   console.log(data, positionData);
+    // })
+    
+  // setTimer(1000).then(() => {
+  //   console.log('Timer done!');
+  // })
+  // console.log('Getting position...');
 }
 
 button.addEventListener('click', trackUserHandler);
