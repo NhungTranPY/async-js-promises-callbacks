4bcd24ca60f85573b6647cf8dd3034b0d7640f52
NhungTranPY <nhungtranpy91@gmail.com>
2023-02-14 16:36:36 +0700

    last commit

--- app.js
+++ app.js
@@ -57,13 +57,13 @@ async function trackUserHandler() {
 
 button.addEventListener('click', trackUserHandler);
 
-Promise.race([getPosition(), setTimer(1000)]).then(data => {
-  console.log(data);
-})
+// Promise.race([getPosition(), setTimer(1000)]).then(data => {
+//   console.log(data);
+// })
 
-Promise.all([getPosition(), setTimer(1000)]).then(promiseData => {
-  console.log(promiseData);
-})
+// Promise.all([getPosition(), setTimer(1000)]).then(promiseData => {
+//   console.log(promiseData);
+// })
 
 Promise.allSettled([getPosition(), setTimer(1000)]).then(promisData => {
   console.log(promisData);
